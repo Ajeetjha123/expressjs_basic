d5c3d70c263f0d2576149f149bb604f8c3238424
Ajeet Kumar Jha <kumarjhaajeet653@gmail.com>
2024-07-31 21:47:09 +0530

    routing funcitonality added

--- index.js
+++ index.js
@@ -2,24 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
-  );
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found!</h1>");
 });
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello From Express</h1>");
-});
-
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
