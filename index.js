3c97fe0ab732eb99c5791028ec1870fa94cf4bf1
Ajeet Kumar Jha <kumarjhaajeet653@gmail.com>
2024-07-31 21:13:23 +0530

    basic get and post

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-import express from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
-app.use((req, res, next) => {
-  console.log("in the middleware");
 
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("in the another middleware");
-  res.send("<h1>Hello From Expressjs</h1>");
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello From Express</h1>");
+});
+
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
